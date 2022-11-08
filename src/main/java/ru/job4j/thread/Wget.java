35de423ecdd5cb0f62ad479cc8d4068733b57874
bogdanove <anoth3rx@gmail.com>
2022-11-08 21:47:01 +0300

    4. Скачивание файла с ограничением. [#144271 #358715]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -4,7 +4,6 @@ import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.time.LocalDateTime;
 
 public class Wget implements Runnable {
     private final String url;
@@ -17,15 +16,23 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
+        var fileNames = url.split("/");
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileNames[fileNames.length - 1])) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             var start = System.currentTimeMillis();
+            int downloadData = 0;
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                downloadData += bytesRead;
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                if (bytesRead < speed) {
-                    Thread.sleep(speed - bytesRead);
+                if (downloadData >= speed) {
+                    var stop = System.currentTimeMillis() - start;
+                    if (stop < 1000) {
+                        Thread.sleep(1000 - stop);
+                    }
+                    downloadData = 0;
+                    start = System.currentTimeMillis();
                 }
             }
         } catch (IOException | InterruptedException e) {
@@ -34,8 +41,21 @@ public class Wget implements Runnable {
         }
     }
 
+    private static boolean validateArgs(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Not enough arguments");
+        }
+        if (!args[0].startsWith("http")) {
+            throw new IllegalArgumentException("Invalid URL");
+        }
+        if (Integer.parseInt(args[1]) <= 0) {
+            throw new IllegalArgumentException("Invalid speed");
+        }
+        return true;
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        if (args.length == 2 && args[0].startsWith("http") && Integer.parseInt(args[1]) > 0) {
+        if (validateArgs(args)) {
             String url = args[0];
             int speed = Integer.parseInt(args[1]);
             Thread wget = new Thread(new Wget(url, speed));
