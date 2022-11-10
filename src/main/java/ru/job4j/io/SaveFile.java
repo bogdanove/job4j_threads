272416e9380bee1bdf8cbb826d4d8e3037fd9917
bogdanove <anoth3rx@gmail.com>
2022-11-10 20:11:39 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102 #359133]

--- src/main/java/ru/job4j/io/SaveFile.java
+++ src/main/java/ru/job4j/io/SaveFile.java
@@ -1,16 +1,19 @@
 package ru.job4j.io;
 
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
-public class SaveFile {
+public final class SaveFile {
+
+    private final File file;
+
+    public SaveFile(File file) {
+        this.file = file;
+    }
 
     public synchronized void saveContent(String content) throws IOException {
         try (var out = new PrintWriter(
                 new BufferedOutputStream(
-                        new FileOutputStream("out.txt")
+                        new FileOutputStream(file)
                 ))) {
             for (var i = 0; i < content.length(); i++) {
                 out.write(content.charAt(i));
